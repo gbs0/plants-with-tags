22e3542066e1cca4de4e65e44583ab3f2b4d31f8
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-11-18 22:39:17 -0300

    finish challenge

---
+++ app/controllers/plant_tags_controller.rb
@@ -0,0 +1,34 @@
+class PlantTagsController < ApplicationController
+  def new
+  	@plant = Plant.find(params[:plant_id])
+  	@plant_tag = PlantTag.new
+  end
+
+  def create
+  	@plant = Plant.find(params[:plant_id])
+  	@tags = Tag.find(params[:plant_tag][:tag])
+  	
+  	# Versão Original
+  	# plant_tag = PlantTag.new
+  	# plant_tag.plant = @plant
+  	# plant_tag.tag = @tag
+  	# plant_tag.save
+
+  	# Refatoração - HTML multiple
+  	@tags.each do |tag|
+  		plant_tag = PlantTag.new
+      	plant_tag.plant = @plant
+      	plant_tag.tag = tag
+      	plant_tag.save
+  		p tag
+  	end
+
+  	redirect_to garden_path(@plant.garden)
+  end
+
+  def destroy
+  	@plant_tag = PlantTag.find(params[:id])
+  	@plant_tag.destroy
+  	redirect_to garden_path(@plant_tag.plant.garden)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  root to: "gardens#index"
   resources :gardens do
     resources :plants, only: [:create]
   end
 
-  resources :plants, only: :destroy
+  resources :plants, only: [:new, :create, :destroy] do
+  	resources :plant_tags, only: [:new, :create]
+  end
+
+  resources :plant_tags, only: [:destroy]
 end
